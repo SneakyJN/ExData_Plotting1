148110e7b0714c02d5107ff06290d6f149d398d0
John Nisi <johnnisi@microsoft.com>
2016-01-13 11:17:08 -0500

    Final check in of R and PNG files

---
+++ plot1.R
@@ -0,0 +1,8 @@
+d <- read.table("household_power_consumption.txt", header=T, sep=";", na.strings="?",stringsAsFactors=F) 
+d <- d[(d$Date == "1/2/2007") | (d$Date == "2/2/2007"),] 
+d$DateTime <- strptime( paste(d$Date, d$Time, sep=" "),format="%d/%m/%Y %H:%M:%S") 
+
+png(filename="plot1.png",width=480, height=480, bg="white") 
+par(mar=c(4.5, 4.5, 2, 1), mfrow=c(1, 1)) 
+hist(d$Global_active_power, col="red", xlab="Global Active Power (kilowatts)", main="Global Active Power") 
+dev.off()
